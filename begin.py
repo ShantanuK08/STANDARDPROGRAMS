e1d18b15344a9af359302f1694feed9ca7d2103d
shantanu <shantanukavathekar5@gmail.com>
2025-01-11 10:52:00 +0530

    go

---
+++ begin.py
@@ -0,0 +1,7 @@
+the_world_is_round = True 
+
+if the_world_is_round :
+    print(the_world_is_round)
+
+
+
---
+++ begin2.py
@@ -0,0 +1,42 @@
+print(2*2)
+
+print(50-5*6)
+
+print((50-5*6)/4)
+
+print(8/5)
+
+print(17/3) # classic division returns a float
+
+print(17//3) # floor division discards the fractional part
+
+print(17%3) # the % operator returns the remainder of the division
+
+print(5*3+2) #floored quotient * divisor + remainder
+
+print(5**2) #square
+
+print(3**4) #3 raised to 4
+
+width = 3
+height = 4 
+print(width*height)
+print(width**height)
+
+
+width = 30
+height = 4 * 5
+print(width*height)
+print(width**height)
+
+
+tax = 12.5/100
+price = 100.50
+print(price*tax)
+_ = 74.3
+print(price + _ )
+print(round(_,2)) #The function round(_, 2) rounds the value of _ to two decimal places.
+
+
+
+
